967eb42feaf1516f33f017c89744f25bfc37f41f
Agata <108717863+Agattoss@users.noreply.github.com>
2022-11-24 17:23:19 +0100

    feat: display product detail

--- src/app/components/product-detail/product-detail.component-module.ts
+++ src/app/components/product-detail/product-detail.component-module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 import { ProductDetailComponent } from './product-detail.component';
 
 @NgModule({
-  imports: [],
+  imports: [MatCardModule, CommonModule],
   declarations: [ProductDetailComponent],
   providers: [],
   exports: [ProductDetailComponent]
